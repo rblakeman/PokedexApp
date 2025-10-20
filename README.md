fca15d36e768e5911a2d03a52b032765a2d03304
Ryan Blakeman <rblakeman31@gmail.com>
2025-10-19 17:19:45 -0700

    typos

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TicTacToe
+# PokedexApp
 
 ## Getting Started
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    // Since this is deployed to `https://rblakeman.github.io/TicTacToe/`
+    // Since this is deployed to `https://rblakeman.github.io/PokedexApp/`
     base: '/PokedexApp/',
     plugins: [react()],
     server: {
